b3d2609bd0b05bf1020047fee79e256bb9fa8760
fa18swiss <fa18swiss@users.noreply.github.com>
2014-12-03 11:04:41 +0100

    Ajout projet de base

---
+++ compilateur.py
@@ -0,0 +1,73 @@
+import AST
+from AST import addToClass
+
+operations = {
+    '+': "ADD",
+    '-': "SUB",
+    '*': "MUL",
+    '/': "DIV",
+}
+
+@addToClass(AST.AssignNode)
+def compile(self):
+    return "%sSET %s\r\n" % (self.children[1].compile(), self.children[0].tok)
+
+@addToClass(AST.TokenNode)
+def compile(self):
+    if isinstance(self.tok, str):
+        return "PUSHV %s\r\n" % self.tok
+    else:
+        return "PUSHC %s\r\n" % self.tok
+
+
+@addToClass(AST.OpNode)
+def compile(self):
+    args = [c.compile() for c in self.children]
+    bytecode = ""
+    if len(args) == 1:
+        bytecode = "PUSHC 0\r\n"
+    for a in args:
+        bytecode += a
+    bytecode += operations[self.op] + "\r\n"
+    return bytecode
+
+
+@addToClass(AST.PrintNode)
+def compile(self):
+    return self.children[0].compile() + "PRINT\r\n"
+
+indexSeq = 0
+@addToClass(AST.WhileNode)
+def compile(self):
+    global indexSeq
+    indexSeq += 1
+    index = indexSeq
+    bytecode = "JMP cond%d\r\nbody%d: " % (index, index)
+    bytecode += self.children[1].compile()
+    bytecode += "cond%d: %s" % (index, self.children[0].compile())
+    bytecode += "JINZ body%d\r\n" % index
+
+    return bytecode
+
+@addToClass(AST.ProgramNode)
+def compile(self):
+    bytecode = ""
+    for c in self.children:
+        bytecode += c.compile()
+
+    return bytecode
+
+
+if __name__ == "__main__":
+    from parser5 import parse
+
+    name = "source.txt"
+    prog = open(name).read()
+    ast = parse(prog)
+    compiled = ast.compile()
+
+    name = "%s.vim" % name
+    outfile = open(name, "w")
+    outfile.write(compiled)
+    outfile.close()
+    print("Write output to %s" % name)
---
+++ parser5.py
@@ -0,0 +1,103 @@
+import ply.yacc as yacc
+from lex5 import tokens
+import AST
+
+
+variabes = {}
+
+#def p_programme_statement(p):
+#    """programme : statement ENDL"""
+#    p[0] = AST.ProgramNode(p[1])
+#
+#
+#def p_programme_recursive(p):
+#    """programme : statement ENDL programme """
+#    p[0] = AST.ProgramNode([p[1]] + p[3].children)
+
+def p_programme_statement(p):
+    """programme : statement ENDL
+    | statement ENDL programme"""
+    try:
+        p[0] = AST.ProgramNode([p[1]] + p[3].children)
+    except:
+        p[0] = AST.ProgramNode(p[1])
+
+
+def p_statement(p):
+    """statement : affectation
+    | structure
+    | PRINT expression"""
+    try:
+        p[0] = AST.PrintNode(p[2])
+    except:
+        p[0] = p[1]
+
+
+def p_structure(p):
+    """structure : WHILE expression '{' programme '}'"""
+    p[0] = AST.WhileNode([p[2], p[4]])
+
+
+def p_affectation(p):
+    """affectation : IDENTIFIANT EQUAL_OP expression """
+    p[0] = AST.AssignNode([AST.TokenNode(p[1]), p[3]])
+
+
+def p_expression_num(p):
+    """expression : NUMBER
+    | IDENTIFIANT """
+    p[0] = AST.TokenNode(p[1])
+
+
+def p_expression_par(p):
+    """expression : "(" expression ")" """
+    p[0] = p[2]
+
+
+operations = {
+    '+': lambda x, y: x+y,
+    '-': lambda x, y: x-y,
+    '*': lambda x, y: x*y,
+    '/': lambda x, y: x/y,
+}
+
+
+def p_expression_op(p):
+    """expression : expression ADD_OP expression
+     | expression MULT_OP expression """
+    p[0] = AST.OpNode(p[2], [p[1], p[3]])
+
+def p_minus(p):
+    """expression : ADD_OP expression %prec UMINUS"""
+    p[0] = AST.OpNode(p[1], [p[2]])
+
+
+def p_error(p):
+    #print("Syntax error in line %d" % p.lineno)
+    print("Syntax error")
+    print(p)
+    yacc.errok()
+
+precedence = (
+    ('left', 'ADD_OP'),
+    ('left', 'MULT_OP'),
+    ('right', 'UMINUS'),
+)
+
+def parse(program):
+    return yacc.parse(program)
+
+yacc.yacc(outputdir='generated')
+
+if __name__ == "__main__":
+    prog = open("test5.txt").read()
+    result = yacc.parse(prog, debug=0)
+    graph = result.makegraphicaltree()
+    name = "test5.pdf"
+    try:
+        import os
+        os.remove(name)
+    except:
+        pass
+    graph.write_pdf(name)
+    print("Wrote ast to " , name)
