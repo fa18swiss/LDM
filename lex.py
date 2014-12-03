a40e851cabc23221a63a67a78a89b0646acf552b
fa18swiss <fa18swiss@users.noreply.github.com>
2014-12-03 11:26:33 +0100

    Dev lex + ajout exemple simple

--- lex.py
+++ lex.py
@@ -1,25 +1,19 @@
+#-*- coding: utf-8 -*-
 import ply.lex as lex
-
-reserved_words = (
-    'while',
-    'print'
-)
 
 tokens = ( 
     'NUMBER',
-    'ADD_OP',
-    'MULT_OP',
+    'OPERATOR',
     'EQUAL_OP',
     'IDENTIFIANT',
     'ENDL'
-) + tuple(map(lambda s : s.upper(), reserved_words))
+)
 
-t_ADD_OP = r'[+-]'
-t_MULT_OP = r'[*/]'
-t_EQUAL_OP = r'[=]'
-t_ENDL = r'[;]'
-
-literals = '(){}'
+t_OPERATOR = r'[¬§\\¦]'
+t_EQUAL_OP = r'[~]'
+t_ENDL = r'\|'
+t_IDENTIFIANT = r'[a-zA-Z_][\w_]*'
+literals = 'éèà£üöä](}['
 
 def t_NUMBER(t):
     r'\d+\.?\d*'
@@ -28,12 +22,6 @@ def t_NUMBER(t):
     except ValueError:
         print ("Lind %d: Problem while parsing %s!" % (t.lineno, t.value))
         t.value = 0
-    return t
-
-def t_IDENTIFIANT(t):
-    r'[a-zA-Z_][\w_]*'
-    if t.value in reserved_words:
-        t.type = t.value.upper()
     return t
 
 def t_newline(t):
@@ -47,12 +35,12 @@ t_ignore = ' \t'
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
+    print(t)
     
 lex.lex()
 if __name__ == "__main__":
-    import sys
-    prog = open("test1.txt").read()
-    #prog = open(sys.argv[1]).read()
+    import codecs
+    prog = codecs.open("test1.txt", "r", "UTF-8").read()
     lex.input(prog)
     while 1:
         tok = lex.token()
